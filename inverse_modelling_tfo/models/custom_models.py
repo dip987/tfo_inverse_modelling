778f7f42081197e1473bb8acc09dfa52c73ac718
dip987 <sa.dip987@gmail.com>
2023-12-17 12:22:48 -0800

    fixed a silly indexing bug in FeatureResidualNetwork

--- inverse_modelling_tfo/models/custom_models.py
+++ inverse_modelling_tfo/models/custom_models.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
-from torch import nn
+import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 
 class SplitChannelCNN(nn.Module):
@@ -27,12 +28,14 @@ class SplitChannelCNN(nn.Module):
         linear_array: List[int] = [2, 1],
     ) -> None:
         super().__init__()
+
         split_error_msg = "cnn_out_channel has to be divisible by split_count"
         assert cnn_out_channel % split_count == 0, split_error_msg
         self.split_count = split_count
         self.inputs_per_split = input_length // split_count
         self.conv1 = nn.Conv1d(split_count, cnn_out_channel, kernel_size, groups=split_count)
         self.conv_output_length = cnn_out_channel * (self.inputs_per_split + 1 - kernel_size)
+        self.linear_layers: List[nn.Module]
         self.linear_layers = [nn.Linear(self.conv_output_length, linear_array[0])]
         for index, count in enumerate(linear_array[0:-1]):
             self.linear_layers.append(nn.ReLU())
@@ -56,6 +59,7 @@ class PerceptronReLU(nn.Module):
 
     def __init__(self, node_counts: List[int]) -> None:
         super().__init__()
+        self.layers: List[nn.Module]
         self.layers = [nn.Linear(node_counts[0], node_counts[1])]
         for index, count in enumerate(node_counts[1:-1], start=1):
             self.layers.append(nn.ReLU())
@@ -75,6 +79,7 @@ class PerceptronBN(nn.Module):
 
     def __init__(self, node_counts: List[int]) -> None:
         super().__init__()
+        self.layers: List[nn.Module]
         self.layers = [nn.Linear(node_counts[0], node_counts[1])]
         for index, count in enumerate(node_counts[1:-1], start=1):
             self.layers.append(nn.BatchNorm1d(count))
@@ -99,6 +104,7 @@ class PerceptronDO(nn.Module):
 
     def __init__(self, node_counts: List[int], dropout_rates: Optional[List[float]] = None) -> None:
         super().__init__()
+        self.layers: List[nn.Module]
         if dropout_rates is None:
             linear_layer_count = len(node_counts) - 1
             dropout_layer_count = linear_layer_count - 1
@@ -123,6 +129,7 @@ class PerceptronBD(nn.Module):
 
     def __init__(self, node_counts: List[int], dropout_rates: Optional[List[float]] = None) -> None:
         super().__init__()
+        self.layers: List[nn.Module]
         if dropout_rates is None:
             linear_layer_count = len(node_counts) - 1
             dropout_layer_count = linear_layer_count - 1
@@ -140,3 +147,77 @@ class PerceptronBD(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+
+class FeatureResidual(torch.nn.Module):
+    def __init__(self, lookup_table: torch.Tensor, lookup_key_indices: torch.Tensor, feature_indices: torch.Tensor):
+        super().__init__()
+        self.lookup_table = lookup_table
+        self.lookup_key_indices = lookup_key_indices
+        self.feature_indices = feature_indices
+
+    def forward(self, predicted_key, features):
+        # dist: distances between each batch(rows) against each key in the lookup table (columns)
+        dist = torch.cdist(predicted_key, self.lookup_table[:, self.lookup_key_indices])
+        # collapse columns to get the index of the closest key for each batch(row), 1D
+        closest_point_index = torch.argmin(dist, dim=1)
+        # How array indices work in torch/numpy: the result has the same shape as the indices. Techinically since I
+        # want a 2D array, the indices should also be 2D, each element would then correspind to (x, y) to capture a
+        # single element. But since I want the same index for each row, I can just use a 1D array and broadcast it.
+        # Same for the columns, I want the same index for each column, so I can just use a 1D array and broadcast it.
+        closest_point_features = self.lookup_table[closest_point_index.view(-1, 1), self.feature_indices.view(1, -1)]
+        return features - closest_point_features
+
+
+class FeatureResidualNetwork(torch.nn.Module):
+    """
+    Generates residuals based on the predicted key and the lookup table, then uses this residual to make a final 
+    predictions. 
+    
+    The network consists of two separate sequential NNs connected at the center by a residual generator. The first NN
+    predicts a set of intermediate labels. The lookup table is used to fetch the features closes to this label
+    combination. The residual generator then generates the difference between the input features and the fetched
+    features. This residual is then passed on to the second NN which makes the final prediction.
+    
+    Arguments:
+    --------
+    node_count_left: List[int] Node counts for the first NN. Its connected to the input on one side and a residual 
+    generator on the other side. The last element must be the number of intermediate features which are passed on to the
+    residual network to make predictions. This would predict our intermediate labels/keys
+    dropout_rates_left: List[float] Dropout rates for the first NN
+    node_count_right: List[int] Node counts for the second NN connected to the residual generator on one side and the
+    output on the other side. The first element must be the number of intermediate labels/keys 
+    dropout_rates_right: List[float] Dropout rates for the second NN
+    lookup_table: 2d torch.Tensor Lookup table containing a set of features and labels, this table is used to look up
+    the intermediate feature values at the predicted key(by the first/left NN)
+    lookup_key_indices: 1d torch.Tensor Indices of the lookup table that correspond to the keys/intermediate lables
+    feature_indices: 1d torch.Tensor Indices of the lookup table that correspond to the input features
+    """
+    def __init__(
+        self,
+        node_count_left: List[int],
+        dropout_rates_left: List[float],
+        node_count_right: List[int],
+        dropout_rates_right: List[float],
+        lookup_table: torch.Tensor,
+        lookup_key_indices: torch.Tensor,
+        feature_indices: torch.Tensor,
+    ):
+        super().__init__()
+        # Check validity
+        assert (
+            node_count_left[-1] == lookup_key_indices.shape[0]
+        ), "Left Node List must end with the number of lookup keys"
+        assert (
+            node_count_right[0] == node_count_left[0]
+        ), "First element of Right Node List must equal to the number of input features"
+
+        # Create the NNs
+        self.left_nn = PerceptronBD(node_count_left, dropout_rates_left)
+        self.feature_residual = FeatureResidual(lookup_table, lookup_key_indices, feature_indices)
+        self.right_nn = PerceptronBD(node_count_right, dropout_rates_right)
+
+    def forward(self, x):
+        path = self.left_nn(x)
+        path = self.feature_residual(path, x)
+        path = self.right_nn(path)
+        return path
---
+++ test/models/test_custom_models.py
@@ -0,0 +1,34 @@
+import unittest
+import torch
+from inverse_modelling_tfo.models.custom_models import FeatureResidualNetwork
+
+
+class TestFeatureResidualNetowrk(unittest.TestCase):
+    def setUp(self):
+        self.in_features = 10
+        self.out_features = 5
+        self.batch_size = 1
+        self.lookup_table = torch.rand(100, self.in_features + self.out_features)
+        self.lookup_key_indices = torch.arange(self.in_features, self.out_features + self.in_features)
+        self.feature_indices = torch.arange(0, self.in_features)
+        self.model = FeatureResidualNetwork(
+            [self.in_features, 4, self.lookup_key_indices.shape[0]],
+            [0.5, 0.5],
+            [self.in_features, 8, self.out_features],
+            [0.5, 0.5],
+            self.lookup_table,
+            self.lookup_key_indices,
+            self.feature_indices,
+        )
+        
+        # Training/Testing with only one row of data messes up BatchNorm's moving-mean calcualtion. Set to eval mode to avoid this
+        self.model = self.model.eval()
+
+    def test_network_produces_output(self):
+        x = torch.rand(self.batch_size, self.in_features)
+        y = self.model(x)
+        self.assertEqual(y.shape, (self.batch_size, self.out_features))
+
+
+if __name__ == "__main__":
+    unittest.main()
