added a notebook + dtype bug fixes for sat. sign detection

inverse_modelling_tfo/data/datasets.py
@@ -165,7 +165,9 @@ class SignDetectionDataset(Dataset):
 
         ## Generate Label
         label = torch.tensor(
-            [1 if (group_y[row1_index].item() > group_y[row1_index].item()) else 0], device=x_data.device
+            [1.0 if (group_y[row1_index].item() > group_y[row1_index].item()) else 0.0],
+            device=x_data.device,
+            dtype=torch.float32,
         )
 
         return x_data, label

inverse_modelling_tfo/model_training/DataLoaderGenerators.py
@@ -21,7 +21,7 @@ def _groupby_to_tensor(
     x_groups = [
         table.iloc[row_indices, col_indices] for row_indices in list(table.groupby(group_columns).groups.values())
     ]
-    x_groups = [torch.tensor(x.values, device=device) for x in x_groups]
+    x_groups = [torch.tensor(x.values, device=device, dtype=torch.float32) for x in x_groups]
     return x_groups
 
 